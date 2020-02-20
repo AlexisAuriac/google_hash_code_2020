a6c94caa1fe550c7d6bfeaccc1cd67110e6f4ccc
alexis <alexis.auriac@epitech.eu>
2020-02-20 22:04:55 +0100

    Output is written to a file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,47 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 mod compute_submission;
 mod get_args;
 mod get_orig_state;
 mod utilities;
 
-use compute_submission::compute_submission;
+use compute_submission::{compute_submission, LibraryScan, Submission};
 use get_args::get_args;
 use get_orig_state::get_orig_state;
 
-// fn write_submission(example_file_name: &str, submission: Submission) {}
+fn lib_scan_to_string(lib_scan: &LibraryScan) -> String {
+    let desc = format!("{} {}", lib_scan.id, lib_scan.books.len());
+    let books_str = lib_scan
+        .books
+        .iter()
+        .map(|b| b.to_string())
+        .collect::<Vec<_>>()
+        .join(" ");
+
+    format!("{}\n{}", desc, books_str)
+}
+
+fn submission_to_string(submission: &Submission) -> String {
+    let first_line = format!("{}", submission.libs.len());
+    let lib_lines = submission
+        .libs
+        .iter()
+        .map(|lib| lib_scan_to_string(lib))
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    format!("{}\n{}\n", first_line, lib_lines)
+}
+
+fn write_submission(example_file_name: &str, submission: &Submission) {
+    let output_file_name = format!("{}.output", example_file_name);
+    let mut file = File::create(output_file_name).unwrap();
+
+    let string_submission = submission_to_string(submission);
+
+    file.write_all(string_submission.as_bytes()).unwrap();
+}
 
 fn main() -> Result<(), String> {
     let args = get_args()?;
@@ -15,7 +49,7 @@ fn main() -> Result<(), String> {
 
     let submission = compute_submission(&orig_state);
 
-    println!("{:?}", submission);
+    write_submission(&args.file_name, &submission);
 
     Ok(())
 }
