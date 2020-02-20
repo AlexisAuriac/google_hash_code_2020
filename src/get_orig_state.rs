9025278a1a36de1d11bdd3df6411e7b0509d441d
alexis <alexis.auriac@epitech.eu>
2020-02-20 20:19:45 +0100

    Parsing done

---
+++ src/get_orig_state.rs
@@ -0,0 +1,135 @@
+use crate::utilities::{load_file, parse_pretty_err};
+
+#[derive(Debug)]
+pub struct LibraryDesc {
+    pub signup_time: usize,
+    pub books: Vec<usize>,
+    pub ship_per_day: usize,
+}
+
+#[derive(Debug)]
+pub struct OrigState {
+    pub nb_books: usize,
+    pub libs: Vec<LibraryDesc>,
+    pub nb_days: usize,
+    pub books: Vec<usize>,
+}
+
+impl OrigState {
+    pub fn new(nb_books: usize, nb_libs: usize, nb_days: usize) -> OrigState {
+        OrigState {
+            nb_books,
+            libs: Vec::with_capacity(nb_libs),
+            nb_days,
+            books: Vec::with_capacity(nb_books),
+        }
+    }
+}
+
+fn parse_first_line(first_line: &str) -> Result<(usize, usize, usize), String> {
+    let line_parts = first_line.split_whitespace().collect::<Vec<_>>();
+
+    if line_parts.len() != 3 {
+        return Err("Invalid first line".to_string());
+    }
+
+    Ok((
+        parse_pretty_err(line_parts[0])?,
+        parse_pretty_err(line_parts[1])?,
+        parse_pretty_err(line_parts[2])?,
+    ))
+}
+
+fn parse_book_line(book_line: &str, books: &mut Vec<usize>) -> Result<(), String> {
+    let line_parts = book_line.split_whitespace().collect::<Vec<_>>();
+
+    for s in line_parts {
+        books.push(parse_pretty_err(s)?);
+    }
+
+    Ok(())
+}
+
+fn zip_libs_with_books(
+    nb_libs: usize,
+    file_lines: &[&str],
+) -> Result<Vec<(String, String)>, String> {
+    if file_lines.len() % 2 != 0 {
+        return Err("Not all libs have a book list".to_string());
+    } else if file_lines.len() / 2 != nb_libs {
+        return Err("There are too many libraries".to_string());
+    }
+
+    let mut libs_and_books = Vec::with_capacity(nb_libs);
+
+    for i in (0..nb_libs * 2).step_by(2) {
+        libs_and_books.push((file_lines[i].to_string(), file_lines[i + 1].to_string()));
+    }
+
+    Ok(libs_and_books)
+}
+
+fn parse_lib_desc(lib_desc: &str) -> Result<(usize, usize, usize), String> {
+    let line_parts = lib_desc.split_whitespace().collect::<Vec<_>>();
+
+    if line_parts.len() != 3 {
+        return Err("Invalid lib description".to_string());
+    }
+
+    Ok((
+        parse_pretty_err(line_parts[0])?,
+        parse_pretty_err(line_parts[1])?,
+        parse_pretty_err(line_parts[2])?,
+    ))
+}
+
+fn parse_lib(lib_desc: &str, books: &str) -> Result<LibraryDesc, String> {
+    let (nb_books, signup_time, ship_per_day) = parse_lib_desc(lib_desc)?;
+    let book_ids = books
+        .split_whitespace()
+        .map(parse_pretty_err)
+        .collect::<Result<Vec<usize>, _>>()?;
+
+    if nb_books != book_ids.len() {
+        return Err("There are more books than was said in the description".to_string());
+    }
+
+    Ok(LibraryDesc {
+        signup_time,
+        books: book_ids,
+        ship_per_day,
+    })
+}
+
+fn parse_file_lines(file_lines: &mut Vec<&str>) -> Result<OrigState, String> {
+    if file_lines.len() < 2 {
+        return Err("Not enough lines to parse".to_string());
+    }
+
+    let (nb_books, nb_libs, nb_days) = parse_first_line(file_lines.remove(0))?;
+    let mut orig_state = OrigState::new(nb_books, nb_libs, nb_days);
+
+    parse_book_line(file_lines.remove(0), &mut orig_state.books)?;
+
+    let libs_and_books = zip_libs_with_books(nb_libs, file_lines)?;
+
+    for (lib_desc, books) in libs_and_books {
+        orig_state.libs.push(parse_lib(&lib_desc, &books)?);
+    }
+
+    Ok(orig_state)
+}
+
+pub fn get_orig_state(file_name: &str) -> Result<OrigState, String> {
+    let file_content = load_file(file_name)?;
+    let mut lines = file_content.split('\n').collect::<Vec<_>>();
+
+    match lines.last() {
+        Some(&"") => {
+            lines.pop();
+            parse_file_lines(&mut lines)
+        }
+        Some(_) => parse_file_lines(&mut lines),
+        _ => Err("Empty file".to_string()),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,15 @@
 mod get_args;
+mod get_orig_state;
 mod utilities;
 
 use get_args::get_args;
-use utilities::{load_file, parse_pretty_err};
-
-#[derive(Debug)]
-struct LibraryDesc {
-    signup_time: usize,
-    books: Vec<usize>,
-    ship_per_day: usize,
-}
-
-#[derive(Debug)]
-struct OrigState {
-    nb_books: usize,
-    libraries: Vec<LibraryDesc>,
-    nb_days: usize,
-}
-
-fn parse_first_line(first_line: &str) -> Result<(usize, usize, usize), String> {
-    let line_parts = first_line.split_whitespace().collect::<Vec<_>>();
-
-    if line_parts.len() != 3 {
-        return Err("Invalid first line".to_string());
-    }
-
-    Ok((
-        parse_pretty_err(line_parts[0])?,
-        parse_pretty_err(line_parts[1])?,
-        parse_pretty_err(line_parts[2])?,
-    ))
-}
-
-fn parse_file_lines(file_lines: &mut std::vec::Vec<&str>) -> Result<OrigState, String> {
-    let (nb_books, nb_libs, nb_days) = parse_first_line(file_lines.remove(0))?;
-
-    println!("nb_books: {:?}", nb_books);
-    println!("nb_libs: {:?}", nb_libs);
-    println!("nb_days: {:?}", nb_days);
-
-    unimplemented!()
-}
-
-fn get_orig_state(file_name: &str) -> Result<OrigState, String> {
-    let file_content = load_file(file_name)?;
-    let mut lines = file_content.split('\n').collect::<Vec<_>>();
-
-    match lines.last() {
-        Some(&"") => {
-            lines.pop();
-            parse_file_lines(&mut lines)
-        }
-        Some(_) => parse_file_lines(&mut lines),
-        _ => Err("Empty file".to_string()),
-    }
-}
+use get_orig_state::get_orig_state;
 
 fn main() -> Result<(), String> {
     let args = get_args()?;
     let orig_state = get_orig_state(&args.file_name)?;
+
+    println!("original state: {:?}", orig_state);
 
     Ok(())
 }
